880def8ddc20d87b42e7ca90e5049da878250d68
Ryan <straightwind0726@gmail.com>
2023-10-26 17:26:35 +0900

    [Update] Add to animation of Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { styled } from 'styled-components';
 import './CSS/index.css';
 import Header from './Components/Header/Header';
 
+const MainWrapper = styled.main`
+    height: 500vh;
+`;
+
 function App() {
     return (
-        <div>
+        <MainWrapper>
             <Header />
-        </div>
+        </MainWrapper>
     );
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import HeaderController from './HeaderController';
 import HeaderSubController from './HeaderSubController';
 import { styled } from 'styled-components';
 
 const HeaderWrapper = styled.header`
+    position: fixed;
+    top: 0;
+    width: 100%;
     padding: 1rem 12rem;
+    transform-origin: bottom center;
+    transition: transform 0.3s ease;
 `;
 
 export default function Header() {
+    const headerRef = useRef(null);
+    const [isHeaderHidden, setHeaderHidden] = useState(false);
+
+    useEffect(() => {
+        const headerHeight = headerRef.current.offsetHeight;
+        const handleScroll = () => {
+            if (window.scrollY > headerHeight) {
+                setHeaderHidden(true);
+            } else {
+                setHeaderHidden(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <HeaderWrapper>
+        <HeaderWrapper
+            ref={headerRef}
+            style={{ transform: isHeaderHidden ? 'translateY(-100%)' : 'none' }}
+        >
             <HeaderController />
             <HeaderSubController />
         </HeaderWrapper>
